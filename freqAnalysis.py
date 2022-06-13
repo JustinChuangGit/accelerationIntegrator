c8aefa10c38afcbb321bffb5668d0db1de2ffd03
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2022-06-13 16:41:37 -0400

    fixed plot function

--- freqAnalysis.py
+++ freqAnalysis.py
@@ -1,13 +1,12 @@
 from sys import displayhook
 import endaq
-
-endaq.plot.utilities.set_theme('endaq_light')
-
 import plotly.express as px
 import pandas as pd
 import numpy as np
 import scipy
 from IPython.display import display
+from plot import plotit as plot
+endaq.plot.utilities.set_theme('endaq_light')
 
 
 
@@ -15,3 +14,4 @@ doc = endaq.ide.get_doc('TCB.IDE')
 
 accel = endaq.ide.to_pandas(doc.channels[8].subchannels[0])
 
+plot(accel, 100000)
---
+++ plot.py
@@ -0,0 +1,32 @@
+from sys import displayhook
+import endaq
+
+endaq.plot.utilities.set_theme('endaq_light')
+
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import scipy
+from IPython.display import display
+
+
+accel_time_labels = dict(
+    xaxis_title_text='',
+    yaxis_title_text='Acceleration (g)',
+    legend_title_text=''
+)
+
+
+def plotit(data,points):
+    fig = endaq.plot.plots.rolling_min_max_envelope(
+    data,
+    desired_num_points=points,
+    plot_as_bars=True,
+    opacity=0.7
+    )
+
+    fig.update_layout(
+        accel_time_labels,
+        title_text='Filtered Time Series with 13M Points',
+    )
+    fig.show()
