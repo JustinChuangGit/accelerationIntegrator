08e35ab812c82a8a5e561584c9e185d93c088f04
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2022-06-13 16:22:52 -0400

    Retrieve Acceleration Data
    channel 8.0 is x acceleration

--- freqAnalysis.py
+++ freqAnalysis.py
@@ -1,3 +1,4 @@
+from sys import displayhook
 import endaq
 
 endaq.plot.utilities.set_theme('endaq_light')
@@ -6,3 +7,13 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 import scipy
+from IPython.display import display
+
+
+doc = endaq.ide.get_doc('TCB.IDE')
+table = endaq.ide.get_channel_table(doc)
+
+
+accel = endaq.ide.to_pandas(doc.channels[8].subchannels[0])
+
+print(accel)
