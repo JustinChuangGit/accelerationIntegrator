36165fcb707f1dfe5768ec4416b84215cfb25f9e
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2022-06-22 14:33:49 -0400

    Updated lowessFilter to call correct column in data frame

--- lowessFilter.py
+++ lowessFilter.py
@@ -5,37 +5,39 @@ import plotly.graph_objects as go # for data visualization
 import plotly.express as px # for data visualization 
 import statsmodels.api as sm # to build a LOWESS model
 from scipy.interpolate import interp1d # for interpolation of new data points
+from plot import plotit as plot
+from IPython.display import display
 
 def filter(df):
-    fig = px.line(df, df.loc[:,'time'], df.loc[:,'X (100g)'])
+    # fig = px.line(df, df.loc[:,'time'], df.loc[:,'X (100g)'])
+ 
+    # # Change chart background color
+    # fig.update_layout(dict(plot_bgcolor = 'white'))
 
-    # Change chart background color
-    fig.update_layout(dict(plot_bgcolor = 'white'))
+    # # Update axes lines
+    # fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
+    #                 zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
+    #                 showline=True, linewidth=1, linecolor='black')
 
-    # Update axes lines
-    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
-                    zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
-                    showline=True, linewidth=1, linecolor='black')
+    # fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
+    #                 zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
+    #                 showline=True, linewidth=1, linecolor='black')
 
-    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
-                    zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
-                    showline=True, linewidth=1, linecolor='black')
+    # # Set figure title
+    # fig.update_layout(title=dict(text="Lowess Filtered Data", 
+    #                             font=dict(color='black')))
 
-    # Set figure title
-    fig.update_layout(title=dict(text="Lowess Filtered Data", 
-                                font=dict(color='black')))
+    # # Update marker size
+    # fig.update_traces(marker=dict(size=3))
 
-    # Update marker size
-    fig.update_traces(marker=dict(size=3))
-
-    fig.show()
+    # fig.show()
     # ------- Select variables -------
     # x values for Linear Regression
-    X=df['X3 distance to the nearest MRT station'].values.reshape(-1,1) # Note, we need X to be a 2D array, hence reshape
+    X=df['time'].values.reshape(-1,1) # Note, we need X to be a 2D array, hence reshape
     # x values for LOWESS
-    x=df['X3 distance to the nearest MRT station'].values 
+    x=df['time'].values 
     # y values for both
-    y=df['Y house price of unit area'].values
+    y=df['X (100g)'].values
 
 
     # ------- Linear Regression -------
@@ -55,32 +57,34 @@ def filter(df):
     y_hat1 = lowess(y, x) # note, default frac=2/3
     y_hat2 = lowess(y, x, frac=1/5)
 
+    display(y_hat1)
+
     # Create a scatter plot
-    fig = px.scatter(df, x=df['X3 distance to the nearest MRT station'], y=df['Y house price of unit area'], 
-                    opacity=0.8, color_discrete_sequence=['black'])
+    # fig = px.scatter(df, x=df['X3 distance to the nearest MRT station'], y=df['Y house price of unit area'], 
+    #                 opacity=0.8, color_discrete_sequence=['black'])
 
-    # Add the prediction line
-    fig.add_traces(go.Scatter(x=x_range, y=y_range, name='Linear Regression', line=dict(color='limegreen')))
-    fig.add_traces(go.Scatter(x=y_hat1[:,0], y=y_hat1[:,1], name='LOWESS, frac=2/3', line=dict(color='red')))
-    fig.add_traces(go.Scatter(x=y_hat2[:,0], y=y_hat2[:,1], name='LOWESS, frac=1/5', line=dict(color='orange')))
+    # # Add the prediction line
+    # fig.add_traces(go.Scatter(x=x_range, y=y_range, name='Linear Regression', line=dict(color='limegreen')))
+    # fig.add_traces(go.Scatter(x=y_hat1[:,0], y=y_hat1[:,1], name='LOWESS, frac=2/3', line=dict(color='red')))
+    # fig.add_traces(go.Scatter(x=y_hat2[:,0], y=y_hat2[:,1], name='LOWESS, frac=1/5', line=dict(color='orange')))
 
-    # Change chart background color
-    fig.update_layout(dict(plot_bgcolor = 'white'))
+    # # Change chart background color
+    # fig.update_layout(dict(plot_bgcolor = 'white'))
 
-    # Update axes lines
-    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
-                    zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
-                    showline=True, linewidth=1, linecolor='black')
+    # # Update axes lines
+    # fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
+    #                 zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
+    #                 showline=True, linewidth=1, linecolor='black')
 
-    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
-                    zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
-                    showline=True, linewidth=1, linecolor='black')
+    # fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
+    #                 zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
+    #                 showline=True, linewidth=1, linecolor='black')
 
-    # Set figure title
-    fig.update_layout(title=dict(text="House Price Based on Distance from the Nearest MRT with Model Predictions", 
-                                font=dict(color='black')))
+    # # Set figure title
+    # fig.update_layout(title=dict(text="House Price Based on Distance from the Nearest MRT with Model Predictions", 
+    #                             font=dict(color='black')))
 
-    # Update marker size
-    fig.update_traces(marker=dict(size=3))
+    # # Update marker size
+    # fig.update_traces(marker=dict(size=3))
 
-    fig.show()
+    # fig.show()
