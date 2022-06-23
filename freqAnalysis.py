d7c343f9a661cdb1e6e0bf815da3a7ff12d93937
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2022-06-23 13:49:17 -0400

    lots of stuff temporary

--- freqAnalysis.py
+++ freqAnalysis.py
@@ -5,52 +5,35 @@ import pandas as pd
 import numpy as np
 import scipy
 from IPython.display import display
-from plot import plotit as plot
 endaq.plot.utilities.set_theme('endaq_light')
 import plotly.graph_objs as go
-from lowessFilter import filter
+from savgolFilter import filter
+import statsmodels.api as sm
 
+from endaqStuff import toNormal, test, retrieveData
+from plot import plotit as plot
+from scipy.signal import savgol_filter
 
 doc = endaq.ide.get_doc('TCB.IDE')
 
-accel = endaq.ide.to_pandas(doc.channels[8].subchannels[0], time_mode='seconds')
+accel = retrieveData("TCB.IDE")
 
-plot(accel, 100000)
 
-psd = endaq.calc.psd.welch(accel, bin_width=1)
-psd['Resultant'] = psd.sum(axis=1)
 
-psd_labels = dict(
-    xaxis_title_text='Frequency (Hz)',
-    yaxis_title_text='Acceleration (g^2/Hz)',
-    legend_title_text='',
-    xaxis_type='log',
-    yaxis_type='log',
-)
+filter(accel, plotBool = True)
+# accel['timestamp'] = accel.index
 
-fig = px.line(psd)
-fig.update_layout(
-    psd_labels,
-    title_text='Power Spectral Density',
-)
 
-oct_psd = endaq.calc.psd.to_octave(psd, fstart=4, octave_bins=3)
-oct_psd.head()
+# filtered = savgol_filter(accel['X (100g)'], 501, 2)
 
-for c in oct_psd.columns:
-  fig.add_trace(go.Scattergl(
-      x=oct_psd.index,
-      y=oct_psd[c],
-      name=c+' Octave',
-      line_width=6,
-      line_dash='dash'
-  ))
 
-#fig.show()
 
-data_df, fig = endaq.plot.octave_spectrogram(accel, window=1, bins_per_octave=20)
-#fig.show()
 
-accel['time'] = accel.index
-print(accel)
-filter(accel)
+
+
+# filteredData = pd.DataFrame(filtered, accel['timestamp'])
+# filteredData.columns = ['X (100g)']
+# data = accel.assign(Y = filteredData['X (100g)'])
+# # data = filteredData.assign(Y  = accel['X (100g)'])
+
+# data = data.rename(columns={'Y':'Y (100g)'})
