388c5c316a377127cea4011261d603174f57986f
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2022-06-23 17:25:10 -0400

    Found that time at which max occurs

--- endaqStuff.py
+++ endaqStuff.py
@@ -33,21 +33,6 @@ def retrieveData(filename):
 
 def nearPeak(data, plusMinus):
     maxVal = abs(data['X (100g)']).max()
-    temp = getIndexes(data,maxVal)
+    temp = data.index[abs(data['X (100g)'])==maxVal].tolist()
     display(temp)
 
-def getIndexes(dfObj, value):
-    ''' Get index positions of value in dataframe i.e. dfObj.'''
-    listOfPos = list()
-    # Get bool dataframe with True at positions where the given value exists
-    result = dfObj.isin([value])
-    # Get list of columns that contains the value
-    seriesObj = result.any()
-    columnNames = list(seriesObj[seriesObj == True].index)
-    # Iterate over list of columns and fetch the rows indexes where value exists
-    for col in columnNames:
-        rows = list(result[col][result[col] == True].index)
-        for row in rows:
-            listOfPos.append((row, col))
-    # Return a list of tuples indicating the positions of value in the dataframe
-    return listOfPos
